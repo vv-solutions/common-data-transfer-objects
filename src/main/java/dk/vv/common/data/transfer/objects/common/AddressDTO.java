674d7154a558cb4bcee8aba880f1e3839743027c
Vincent Buchholz <vbu@miracle.dk>
2023-12-14 12:00:30 +0100

    adds AddressDTO

---
+++ src/main/java/dk/vv/common/data/transfer/objects/common/AddressDTO.java
@@ -0,0 +1,47 @@
+package dk.vv.common.data.transfer.objects.common;
+
+public class AddressDTO {
+    private int id;
+
+    private int zipCode;
+
+    private String city;
+
+    private String address;
+
+    public AddressDTO() {
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
